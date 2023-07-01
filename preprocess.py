723401a703a000dd3477be45afd266249f52683c
Joon <joolee@g.hmc.edu>
2023-07-01 16:48:53 -0700

    First round of training models complete

--- preprocess.py
+++ preprocess.py
@@ -1,33 +1,47 @@
 import os
 import librosa
 import json
+import numpy as np
+import random
 
-# This file standardizes the set of audio data by making them all exactly one
-# minute long. If the file is:
-# 1) Less than one minute long, it will be looped to be exactly one minute
+# This file standardizes the set of audio data by making them all exactly ten
+# seconds long. If the file is:
+# 1) Less than 10 seconds long, it will be looped to be exactly 10 seconds
 #    long.
-# 2) Greater than one minute long, it will be split at the one minute mark and
-#    the remaining data will be handled like case 1.
-# 3) Multiple minutes long, it will be divided equally into one minute segments
-#    and the last clip will be handled like case 1.
-
-# Run with
-# `python preprocess.py`
-# 
+# 2) Greater than 10 seconds long but less than 20, it will be split at
+#    the 10 second mark and the remaining data will be handled like case 1.
+# 3) Greater than 20 seconds long, it will be divided equally into 10 second
+#    segments and the last clip will be handled like case 1.
+ 
 # Based off of
 # https://github.com/musikalkemist/DeepLearningForAudioWithPython/blob/master/12-%20Music%20genre%20classification%3A%20Preparing%20the%20dataset/code/extract_data.py
 # 
 # Youtube course video
-# https://www.youtube.com/watch?v=_xcFAiufwd0&list=PL-wATfeyAMNrtbkCNsLcpoAyBBRJZVlnf&index=13
+# https://youtu.be/szyGiObZymo
 
 DATASET_PATH = ""
-SAMPLE_RATE = 22050
-NUM_SAMPLES_ONE_MIN = SAMPLE_RATE * 60
+SAMPLE_RATE = 22050 # per second
+DATA_WINDOW = 10 # seconds
+NUM_SAMPLES_ONE_WINDOW = SAMPLE_RATE * DATA_WINDOW
 JSON_PATH = ""
 DATA = {
         "mapping": [],
         "labels": [],
         "mfcc": []
+    }
+
+# This dictionary helps when we bootstrap the data. It stores the factor by
+# which we need to bootstrap music from a certain area so that we have
+# approximately the same amount of data from each area.
+LABEL_WEIGHT_DICT = {
+        "Dragonspine": 5,
+        "Inazuma": 1,
+        "Mondstadt": 3,
+        "SumeruDesert": 1,
+        "Enkanomiya": 6,
+        "Liyue": 1,
+        "Ocean": 6,
+        "SumeruRainforest": 1
     }
 
 def preprocess(dataset_path):
@@ -40,6 +54,13 @@ def preprocess(dataset_path):
             label = dirpath.split("\\")[-1]
             labelIndex = i - 1
 
+            # Bootstrap the data since the number of songs we have from each
+            # game area is not the same
+            bootstrappedFileNames = []
+            lenFilenames = len(filenames)
+            while len(bootstrappedFileNames) < lenFilenames * LABEL_WEIGHT_DICT[label]:
+                bootstrappedFileNames.append(filenames[random.randrange(lenFilenames)])
+
             for file in filenames:
 
                 filePath = os.path.join(dirpath, file)
@@ -51,59 +72,60 @@ def preprocess(dataset_path):
                 signal, _ = librosa.load(filePath, sr=SAMPLE_RATE)
                 signalLenSeconds = int(librosa.get_duration(y=signal, sr=SAMPLE_RATE))
 
-                # If longer than 1 min, send to splitIntoMinutes
-                if signalLenSeconds > 60:
-                    splitIntoMinutes(signal, label, labelIndex)
-                # If less than 1 min, send to loopToOneMinute
-                elif signalLenSeconds < 60:
-                    loopToOneMinute(signal, label, labelIndex)
-                # The song is less than 61 seconds long, leave as is
-                else:
-                    storeSong(signal, label, labelIndex)
+                for j in range(LABEL_WEIGHT_DICT[label]):
+                    # If longer than 1 window, send to splitIntoMinutes
+                    if signalLenSeconds > DATA_WINDOW:
+                        splitIntoWindows(signal, label, labelIndex)
+                    # If less than 1 min, send to loopToOneMinute
+                    elif signalLenSeconds < DATA_WINDOW:
+                        loopToOneWindow(signal, label, labelIndex)
+                    # The song is less than 61 seconds long, leave as is
+                    else:
+                        storeSong(signal, label, labelIndex)
     
     # save MFCCs to json file
     with open(JSON_PATH, "w") as fp:
         json.dump(DATA, fp, indent=4)
 
-def loopToOneMinute(song, label, labelIndex):
+def loopToOneWindow(song, label, labelIndex):
     loopedSong = []
-    while (len(loopedSong) < NUM_SAMPLES_ONE_MIN):
-        loopedSong.append(song)
+    while (len(loopedSong) < NUM_SAMPLES_ONE_WINDOW):
+        loopedSong.extend(song)
 
-    print("Looped Song length: " + str(len(loopedSong)))
+    storeSong(np.array(loopedSong), label, labelIndex)
 
-    storeSong(loopedSong, label, labelIndex)
-
-def splitIntoMinutes(song, label, labelIndex):
+def splitIntoWindows(song, label, labelIndex):
     secondsLength = librosa.get_duration(y=song, sr=SAMPLE_RATE)
-    minuteIntervals = [(i * NUM_SAMPLES_ONE_MIN, (i + 1) * NUM_SAMPLES_ONE_MIN) for i in range(0, int(secondsLength / 60))]
+    segmentIntervals = [(i * NUM_SAMPLES_ONE_WINDOW, (i + 1) * NUM_SAMPLES_ONE_WINDOW) for i in range(0, int(secondsLength / DATA_WINDOW))]
 
     i = 0
-    for interval in minuteIntervals:
-        print("Storing Minute " + str(i) + " of song.")
+    for interval in segmentIntervals:
+        print("Storing Segment " + str(i) + " of song.")
+
         # Get data for the relevant interval
         intervalSong = song[interval[0]:interval[1]]
+
         storeSong(intervalSong, label, labelIndex)
         i += 1
 
-    lastSong = song[int(secondsLength / 60):int(secondsLength) % 60]
+    lastSong = song[int(secondsLength / DATA_WINDOW):int(secondsLength) % DATA_WINDOW]
 
     # Only create a new looped song if the remaining audio data is significant
-    # which we define here to be greater than 10 seconds long.
-    if (len(lastSong) > SAMPLE_RATE * 10):
-        loopToOneMinute(lastSong, label, labelIndex)
+    # which we define here to be greater than 20% of the data window size.
+    if (len(lastSong) > SAMPLE_RATE * DATA_WINDOW / 5):
+        loopToOneWindow(lastSong, label, labelIndex)
 
 def storeSong(song, label, labelIndex):
     # Store song into training data folder
     print("Storing song of length: " + str(len(song)))
-    truncatedSong = song[:NUM_SAMPLES_ONE_MIN]
+    truncatedSong = song[:NUM_SAMPLES_ONE_WINDOW]
 
     DATA["mapping"].append(label)
     DATA["labels"].append(labelIndex)
 
     # Extract Mel Frequency Cepstral Coefficients
     mfcc = librosa.feature.mfcc(y=truncatedSong, sr=SAMPLE_RATE, n_mfcc=13, n_fft=2048, hop_length=512)
-    print("MFCC length: " + str(len(mfcc.tolist()[0])))
+
     DATA["mfcc"].append(mfcc.tolist())
 
 
--- youtube_downloader.sh
+++ youtube_downloader.sh
@@ -3,16 +3,63 @@
 
 mkdir MusicData
 cd MusicData/
+
 # Mondstadt
+mkdir Mondstadt
+cd Mondstadt/
+youtube-dl --ignore-errors --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail https://youtube.com/playlist?list=PLP2Zo4nJwCT0iD2rtznph5p3sXGo7j-IL
+cd ..
 
 # Dragonspine
 mkdir Dragonspine
 cd Dragonspine/
-youtube-dl --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail https://youtube.com/playlist?list=PLP2Zo4nJwCT1d3emY6Otllgv7nS3ny5V4
+youtube-dl --ignore-errors --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail https://youtube.com/playlist?list=PLP2Zo4nJwCT1d3emY6Otllgv7nS3ny5V4
 cd ..
 
 # Liyue
+mkdir Liyue
+cd Liyue/
+youtube-dl --ignore-errors --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail https://youtube.com/playlist?list=PLP2Zo4nJwCT1KKhS4B_Mz9xcTxVMHXaji
+cd ..
+
 # Chasm
+mkdir Chasm
+cd Chasm/
+youtube-dl --ignore-errors --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail https://youtube.com/playlist?list=PLP2Zo4nJwCT0DpKGwfg03hkotEoPY9DCM
+cd ..
+
 # Inazuma
+mkdir Inazuma
+cd Inazuma/
+youtube-dl --ignore-errors --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail https://youtube.com/playlist?list=PLP2Zo4nJwCT2I6Ylrv6DXsq1x6ix7Pe3k
+cd ..
+
 # Sumeru Rainforest
+mkdir SumeruRainforest
+cd SumeruRainforest/
+youtube-dl --ignore-errors --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail https://youtube.com/playlist?list=PLP2Zo4nJwCT17GobBqo0e2ml3XRcB2veI
+cd ..
+
 # Sumeru Desert
+mkdir SumeruDesert
+cd SumeruDesert/
+youtube-dl --ignore-errors --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail https://youtube.com/playlist?list=PLP2Zo4nJwCT1Yh34NEyIJhXVcfeHrnDjq
+cd ..
+
+# Ocean
+mkdir Ocean
+cd Ocean/
+youtube-dl --ignore-errors --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail https://youtube.com/playlist?list=PLP2Zo4nJwCT1pV4YGsiqDdsYl-PD7gxa-
+cd ..
+
+# Enkanomiya
+mkdir Enkanomiya
+cd Enkanomiya/
+youtube-dl --ignore-errors --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail https://youtube.com/playlist?list=PLP2Zo4nJwCT1bmyU1kPi1p9VwOD2ZP8ov
+cd ..
+
+# Other
+mkdir Other
+cd Other/
+youtube-dl --ignore-errors --prefer-ffmpeg --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail https://youtube.com/playlist?list=PLP2Zo4nJwCT1heLZ9COXk-_oLERCcKVgt
+cd ..
